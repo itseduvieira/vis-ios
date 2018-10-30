4c4397eb5845b20b6f4f394f82a210ef77f652ea
Eduardo Vieira <eduardofelipevieira@gmail.com>
2018-10-30 03:14:36 -0300

    My campaigns

--- VisualNoAr/Campaign/HistoryController.swift
+++ VisualNoAr/Campaign/HistoryController.swift
@@ -7,15 +7,24 @@
 //
 
 import UIKit
+import PromiseKit
 
-class HistoryController: UIViewController {
+class HistoryController: UIViewController,  UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var navBar: UINavigationBar!
     @IBOutlet weak var navItem: UINavigationItem!
+    @IBOutlet weak var table: UITableView!
+    
+    var campaigns: [Campaign]! = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.setNavigationBar()
+        
+        table.dataSource = self
+        table.delegate = self
+        
+        self.getData()
     }
     
     func setNavigationBar() {
@@ -26,8 +35,48 @@ class HistoryController: UIViewController {
         navItem.leftBarButtonItem = backItem
     }
     
+    func getData() {
+        firstly {
+            DataAccess.instance.listCampaigns()
+        }.done { campaigns in
+            self.campaigns = campaigns
+            
+            self.table.reloadData()
+        }.catch { error in
+            print(error)
+        }
+    }
+    
     @objc func back() {
         let vc = self.storyboard?.instantiateViewController(withIdentifier: "MapViewController") as! MapController
         self.present(vc, animated: true, completion: {})
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.campaigns.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CampaignCell", for: indexPath) as! CampaignCustomCell
+        
+        let campaign = self.campaigns[indexPath.row]
+        
+        cell.name.text = campaign.name
+        cell.plane.text = campaign.plane
+        cell.place.text = campaign.place.title
+        cell.featuredLocations.text = "\(campaign.place.urls[0]["file"]!) • \(campaign.place.urls[1]["file"]!) • \(campaign.place.urls[2]["file"]!)"
+        
+        cell.circle.setRadius()
+        
+        return cell
+    }
+}
+
+class CampaignCustomCell: UITableViewCell {
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var circle: UIView!
+    @IBOutlet weak var place: UILabel!
+    @IBOutlet weak var featuredLocations: UILabel!
+    @IBOutlet weak var plane: UILabel!
+    
 }
